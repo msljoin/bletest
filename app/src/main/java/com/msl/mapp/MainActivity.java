9ef9b124122a4843af53f53cc074a324ccfa8e8c
msljoin <dybno.mg@gmail.com>
2021-01-05 19:09:30 +0200

    Add click listeners to buttons - > on, off, find and get pair, onActivityResult to change icon status and show toast info

--- app/src/main/java/com/msl/mapp/MainActivity.java
+++ app/src/main/java/com/msl/mapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.msl.mapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,10 +13,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_ENABLE = 0;
-    private static final int REQUEST_DISCOVER = 1;
+    private static final int REQUEST_ENABLE_BT = 0;
+    private static final int REQUEST_DISCOVER_BT = 1;
 
     private TextView mStatus, mPair;
     private ImageView mStatusIcon;
@@ -39,11 +43,12 @@ public class MainActivity extends AppCompatActivity {
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
         checkBluetoothAdapterIsNull(bluetoothAdapter);
-
-        enableBluetooth(mTurnOn);
-
         checkBluetoothAdapterIsEnabled(bluetoothAdapter);
 
+        enableBt(mTurnOn);
+        disableBt(mTurnOff);
+        discoverBt(mDiscover);
+        pairedBt(mPaired);
     }
 
     private void checkBluetoothAdapterIsNull(BluetoothAdapter adapter) {
@@ -67,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
 
-    private void enableBluetooth(Button button) {
+    private void enableBt(Button button) {
 
         button.setOnClickListener(view -> {
 
@@ -76,11 +81,62 @@ public class MainActivity extends AppCompatActivity {
                 showMessage("Turning on bluetooth");
 
                 Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(intent, REQUEST_ENABLE);
+                startActivityForResult(intent, REQUEST_ENABLE_BT);
             }
             else { showMessage("Bluetooth is already on"); }
         });
     }
 
+    private void discoverBt(Button button) {
 
+        button.setOnClickListener(v -> {
+            if (!bluetoothAdapter.isDiscovering()) {
+
+                showMessage("Making Your Device Discoverable");
+                Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                startActivityForResult(intent, REQUEST_DISCOVER_BT);
+            }
+        });
+    }
+
+    private void disableBt(Button button) {
+
+        button.setOnClickListener(v -> {
+            if (bluetoothAdapter.isEnabled()) {
+
+                bluetoothAdapter.disable();
+                showMessage("Turning  Bluetooth off");
+                mStatusIcon.setImageResource(R.drawable.ic_baseline_bluetooth_disabled_128);
+            } else { showMessage("Bluetooth is already off"); }
+        });
+    }
+
+    private void pairedBt(Button button) {
+
+       button.setOnClickListener(v -> {
+
+           if (bluetoothAdapter.isEnabled()) {
+
+               mPair.setText(R.string.paired_dev);
+               Set<BluetoothDevice> devices = bluetoothAdapter.getBondedDevices();
+
+               for (BluetoothDevice device : devices) {
+                   mPair.append("\n Device : " + device.getName() + " , " + device);
+               }
+           } else { showMessage("Turn On bluetooth to get paired devices"); }
+       });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == RESULT_OK) {
+
+                mStatusIcon.setImageResource(R.drawable.ic_baseline_bluetooth_128);
+                showMessage("Bluetooth is On");
+            } else { showMessage("Bluetooth is Off"); }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
